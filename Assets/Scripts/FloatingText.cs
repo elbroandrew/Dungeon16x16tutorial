938fcd0d221120b78537eb6e6f4fbfafc91f12a5
elbroandrew <elbroandrew@gmail.com>
2022-06-08 09:29:04 +1000

    Floating Text start

---
+++ Assets/Scripts/FloatingText.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingText : MonoBehaviour
+{
+    public bool active;
+    public GameObject go;
+    public Text txt;
+    public Vector3 motion;
+    public float duration;
+    public float lastShown;
+
+    public void Show()
+    {
+        active = true;
+        lastShown = Time.time;
+        go.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        active = false;
+        go.SetActive(active);
+    }
+
+    private void UpdateFloatingText()
+    {
+        if (!active)
+        {
+            return;
+        }
+
+        if (Time.time - lastShown > duration)
+        {
+            Hide();
+        }
+
+        go.transform.position += motion * Time.deltaTime;
+
+    }
+}
